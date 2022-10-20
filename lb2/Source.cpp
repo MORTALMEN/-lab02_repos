a673c014c401da64bcd569a49ae638a11cc406d2
MORTALMEN <maksimstelmach90@gmail.com>
2022-10-20 17:12:23 +0300

    first master commit

---
+++ lb2/Source.cpp
@@ -0,0 +1,26 @@
+//Lab_2.1.cpp
+//<Стельмах Максим Іванович>
+//Лабораторна робота №2.1
+//Лінійне програмуванння 
+//Варіант 28
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x;  // вхідний параметр 
+	double y;  // вхідний параметр
+	double z1;  // результат обчислення 1-го виразу 
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "x="; cin >>x;
+	cout << "y="; cin >>y;
+	z1 = cos(x) * cos(x) * cos(x) * cos(x) + sin(y) * sin(y) + 1 / 4. * sin(2 * x) * sin(2 * x) - 1;
+	z2 = sin(y + x) * sin(y - x);
+		cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
